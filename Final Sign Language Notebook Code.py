ce0d778aa070d3e7330928a6b794954f84eeaeec
KR22041838 <120821258+KR22041838@users.noreply.github.com>
2023-06-30 14:32:31 +0100

    Final Sign Language Notebook Code.py

--- Final Sign Language Notebook Code.py
+++ Final Sign Language Notebook Code.py
@@ -1,3 +1,21 @@
+# This code is base off MediaPipe's Gesture Recogniser model and has been adapeted using Chat GTP 
+#Gesture Recogniser:
+# https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/gesture_recognizer/python/gesture_recognizer.ipynb#scrollTo=KHqaswD6M8iO
+#Gesture Recogniser Documentation:
+# https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer/python
+
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -109,24 +127,6 @@ plt.show()
 print(detection_result.hand_landmarks)
 
 
-# In[ ]:
-
-
-
-
-
-# In[4]:
-
-
-# STEP 3: Load the input image.
-image = mp.Image.create_from_file("signA.jpg")
-
-# STEP 4: Detect hand landmarks from the input image.
-detection_result = detector.detect(image)
-
-
-
-# In[ ]:
 
 
 
